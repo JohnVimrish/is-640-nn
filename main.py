db561a5bf8e07696b29e390a3a183f096ae5ab49
John Vimrish <john.vimri@gmail.com>
2024-11-01 20:58:19 -0700

    main code changes

--- main.py
+++ main.py
@@ -4,14 +4,12 @@ x = [2.0, 3.8,-19.3]
 n = m(3,[4,4,1])
 print(n(x))
 
-xs = [[3.0,-1.0,2.5],[2.0, 3.8,-19.3],[-1.0,3.5,23],[23.5,6.9,-7.3]]
-xy = [1.0,-1.0,-1.0,1.0]
-ys  = [1.0,-1.0,-1.0,1.0]
-
+xs = [[3.0,-1.0,2.5],[2.0, 0.8,-1.3],[-1.5,0.5,2.3],[3.5,6.9,-0.3]]
+ys  = [1.0,-4.0,-1.0,1.0]
 
 for k in range (20) :
     ypredictability = [n(x) for x in xs ]
-    loss =   sum([(yout-  yin )**2 for  yout ,yin in zip(ys,ypredictability)])
+    loss =   sum([(yout-yin)**2 for  yout ,yin in zip(ys,ypredictability)])
 
     for p in n.parameters() :
         p.grad = 0.0
@@ -22,3 +20,6 @@ for k in range (20) :
         p.data =+ 0.1 *  p.grad
     
     print(k,loss.data)
+
+
+print (ypredictability)
